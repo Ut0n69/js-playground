17d8aff65ed76d9959c8a9774bf9cf211a918263
yut0n <yt.sa.1117@gmail.com>
2019-05-13 00:33:47 +0900

    Fix: auto format by prettier

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,9 @@
 import { Greeting } from '@/scripts/js/Greeting'
 import { str } from '@/scripts/ts/string'
-
 import '@/assets/css/style.css'
-
-(() => {
+;(() => {
   console.log('str: ', str)
-  
+
   const greeting = new Greeting('Caroline')
-  greeting.sayHi();
+  greeting.sayHi()
 })()
