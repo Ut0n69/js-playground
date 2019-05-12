e1f538a9b104aaf768423079709ad9912328c79c
yut0n <yt.sa.1117@gmail.com>
2019-05-13 00:32:59 +0900

    Remove: eslint-loader at webpack

--- webpack.config.js
+++ webpack.config.js
@@ -17,15 +17,6 @@ module.exports = {
   module: {
     rules: [
       {
-        enforce: "pre",
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader",
-        options: {
-          fix: true,
-        }
-      },
-      {
         test: /\.js$/,
         use: [{
           loader: "babel-loader",
